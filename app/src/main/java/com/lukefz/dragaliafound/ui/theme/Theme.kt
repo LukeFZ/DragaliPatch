7412b9747377ec61485ea0cfc403910bbf19b7c4
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2022-12-05 01:18:12 +0100

    Make status bar the background color

--- app/src/main/java/com/lukefz/dragaliafound/ui/theme/Theme.kt
+++ app/src/main/java/com/lukefz/dragaliafound/ui/theme/Theme.kt
@@ -1,19 +1,13 @@
 package com.lukefz.dragaliafound.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -55,10 +49,13 @@ fun DragaliPatchTheme(
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController((view.context as Activity).window, view).isAppearanceLightStatusBars = darkTheme
+            //(view.context as Activity).window.statusBarColor = colorScheme.background.toArgb()
+            //WindowCompat.getInsetsController((view.context as Activity).window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
+
+    val uiController = rememberSystemUiController()
+    uiController.setSystemBarsColor(color = colorScheme.background)
 
     MaterialTheme(
         colorScheme = colorScheme,
